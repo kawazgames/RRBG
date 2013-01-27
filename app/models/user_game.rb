374a4981910fc6d257ae8d2dea2a7b8b3e8a97a3
miio <info@miio.info>
2013-01-27 16:08:38 +0900

    refs #23 add score logic

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -1,7 +1,7 @@
 class UserGame < ActiveRecord::Base
   belongs_to :user
   belongs_to :game_stage
-  attr_accessible :status, :user, :user_id, :game_stage, :game_stage_id
+  attr_accessible :status, :user, :user_id, :game_stage, :game_stage_id, :score
 
   def convert_collection
     map = GameMap.where(user_game_stage: self).all(lock: true)
@@ -15,9 +15,19 @@ class UserGame < ActiveRecord::Base
     responed = { GameStage::STATE_PLAYING => "playing", GameStage::STATE_GAMEOVER => "gameover" }
     game_stage = GameStage.where(id: self.game_stage_id).first
     responed_stage = game_stage.set_and_step(fungus_x, fungus_y)
+    @score = game_stage.get_score
+    UserGame.transaction do
+      ug = UserGame.find(self.id, lock: true)
+      if ug.score.nil?
+        ug.score = 0
+      end
+      ug.score = ug.score + @score
+      ug.save!
+      @score = ug.score
+    end
     {
       state: responed[game_stage.play_status],
-      score: game_stage.get_score,
+      score: @score,
       game_stage: responed_stage
     }
   end
