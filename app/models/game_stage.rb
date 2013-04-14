c85d44305b2ec5d8ee943c9fb298ada57da6488b
miio <info@miio.info>
2013-04-14 15:18:09 +0900

    many fix

--- app/models/game_stage.rb
+++ app/models/game_stage.rb
@@ -10,9 +10,9 @@ class GameStage < ActiveRecord::Base
 
   STATE_PLAYING = 0
   STATE_GAMEOVER = 1
-  CLEAR_RATION = 70
+  CLEAR_RATION = 25
 
-  MOVE_LEUKOCYTE = 50
+  MOVE_LEUKOCYTE = 10
 
   def self.create_copy_with_random
     size = Stage.count
@@ -31,26 +31,45 @@ class GameStage < ActiveRecord::Base
     end
     @res
   end
+
+  ############################################################
+  # Interface Structure
+  ############################################################
   def convert_collection
     map = GameMap.where(game_stage_id: self.id).all(lock: true)
     {
       id: self.id,
       game_map: GameMap.convert_collection(self.id)
-
     }
   end
+
+  def convert_structure
+  {
+    newvirus: @new_fungus.map{|m| {x: m.x, y: m.y}},
+    leukocyte: @moved_cleaner,
+    virus: @moved_enemy
+  }
+  end
+
 
   ############################################################
   # Logic of turn
   ############################################################
-
-
   def play_status
     fungus = UserFungus.where(game_stage_id: self.id, status: UserFungus::LIFE).count(lock: "LOCK IN SHARE MODE")
     return STATE_GAMEOVER if fungus >= CLEAR_RATION
     return STATE_PLAYING
   end
 
+  def get_score
+    fungus = UserFungus.where(game_stage_id: self.id, status: UserFungus::LIFE).count(lock: true)
+    leukocyte = EnemyLeukocyte.where(game_stage_id: self.id, status: EnemyLeukocyte::LIFE).count(lock: true)
+    return ((leukocyte) * (fungus * 1000 / (ONE_LINE * ONE_LINE))) + 20
+  end
+
+  ############################################################
+  # Panels
+  ############################################################
   def get_way (v,t)
     y = t[:y] - v[:y]
     x = t[:x] - v[:x]
@@ -67,17 +86,22 @@ class GameStage < ActiveRecord::Base
 
   def near_spaces(i,j)
     nears(i,j).select do |e|
-      !@enemys[e[0]][e[1]] && !@field[e[0]][e[1]]
+      false == (@enemys_obj[e[0]][e[1]] or @field_obj[e[0]][e[1]].is_wall?)
     end
   end
 
   def duplicate_at(i,j)
     t = near_spaces(i,j).sample(1)[0]
-    @enemys[t[0]][t[1]] = true if t
-    UserFungus.create! game_stage: self, y: t[0], x: t[1]
-    @new_fungus << UserFungus.new(game_stage: self, y: t[0], x: t[1])
+    if t
+      @enemys[t[0]][t[1]] = true
+      @enemys_obj[t[0]][t[1]] = UserFungus.new game_stage: self, y: t[0], x: t[1]
+      @new_fungus << @enemys_obj[t[0]][t[1]]
+    end
   end
 
+  #################################################################
+  # TurnPhaseManager
+  #################################################################
   def set_and_step(y, x)
     GameStage.transaction do
       @new_fungus = []
@@ -86,34 +110,44 @@ class GameStage < ActiveRecord::Base
     end
     self.convert_structure
   end
+
   def step()
-
     self.convert_step
+    self.fungus_phase
+    self.leukocyte_phase
+  end
 
+  def fungus_phase
     @moved_enemy = []
     ls = @enemys.each_with_index.map {|x,i|
       x.each_with_index.map {|v,j|
         {i:i, j:j, c:near_spaces(i,j).length} if v
       }.select{|x| x}
     }.reduce(&:+)
+
     ls.sort_by{|a| a[:c] }.each{|x|
       t = near_spaces(x[:i], x[:j]).sample(1)[0]
       if t
         @enemys[t[0]][t[1]] = true
-        @enemys[x[:i]][x[:j]] = false
+
+        # push out field
         @enemys_obj[x[:i]][x[:j]].update_attributes!(y: t[0], x: t[1])
-        @moved_enemy << { y:t[0], x:t[1], way: WAY_STOP }
-        duplicate_at(t[0], t[1])
+        @new_fungus << @enemys_obj[x[:i]][x[:j]]
+        @moved_enemy << { y:x[:i], x:x[:j], way: self.get_way({y: x[:i], x: x[:j]},{y: t[0], x: t[1]}) }
+
+        # Create new fungus for pushed pocket.
+        @new_fungus << UserFungus.new(game_stage: self, y: x[:i], x: x[:j])
       else
         @moved_enemy << { y:x[:i], x:x[:j], way: WAY_STOP }
-
       end
-
     }
 
     # TODO use activerecord-import
     @new_fungus.map{|m| m.save! }
 
+  end
+
+  def leukocyte_phase
     @moved_cleaner = []
     @cleaner.each_with_index do |v, ci|
     way = []
@@ -122,7 +156,7 @@ class GameStage < ActiveRecord::Base
       died_ids = []
       @enemys[v[:y]][v[:x]] = false
       MOVE_LEUKOCYTE.times do
-        t = nears(v[:y], v[:x]).select{|x| !@field[x[0]][x[1]] }.sample(1)[0]
+        t = nears(v[:y], v[:x]).select{|x| !@field_obj[x[0]][x[1]].is_wall? }.sample(1)[0]
         nv = { y: t[0], x: t[1] }
         if @enemys_obj[nv[:y]][nv[:x]]
           @enemys[nv[:y]][nv[:x]] = false
@@ -136,6 +170,7 @@ class GameStage < ActiveRecord::Base
       UserFungus.where(id: died_ids).update_all(status: EnemyLeukocyte::DIED) unless died_ids.empty?
       @cleaner_obj[fv[:y]][fv[:x]].update_attributes!(y: nv[:y], x: nv[:x])
     end
+
   end
 
   def convert_step
@@ -150,6 +185,7 @@ class GameStage < ActiveRecord::Base
       @field[f.y][f.x] = false
       @field_obj[f.y][f.x] = f
     end
+
     ONE_LINE.times do |x|
       ONE_LINE.times do |y|
         @enemys[y][x] = false
@@ -161,7 +197,6 @@ class GameStage < ActiveRecord::Base
       if e.y and e.x
         @enemys[e.y][e.x] = true
         @enemys_obj[e.y][e.x] = e
-        UserFungus.create! game_stage: self, y: e.y, x: e.x
       end
     end
     cleaner = EnemyLeukocyte.where(game_stage_id: self.id, status: EnemyLeukocyte::LIFE).all(lock: true)
@@ -173,17 +208,4 @@ class GameStage < ActiveRecord::Base
     end
   end
 
-  def convert_structure
-  {
-    newvirus: @new_fungus.map{|m| {x: m.x, y: m.y}},
-    leukocyte: @moved_cleaner,
-    virus: @moved_enemy
-  }
-  end
-
-  def get_score
-    fungus = UserFungus.where(game_stage_id: self.id, status: UserFungus::LIFE).count(lock: true)
-    leukocyte = EnemyLeukocyte.where(game_stage_id: self.id, status: EnemyLeukocyte::LIFE).count(lock: true)
-    return ((leukocyte) * (fungus * 1000 / (ONE_LINE * ONE_LINE))) + 20
-  end
 end
