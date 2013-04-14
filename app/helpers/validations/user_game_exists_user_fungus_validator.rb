75a1d126068a001a02a9372a5aa91e4194418aab
miio <info@miio.info>
2013-04-14 15:19:53 +0900

    remove validation

--- app/helpers/validations/user_game_exists_user_fungus_validator.rb
+++
@@ -1,8 +0,0 @@
-class UserGameExistsUserFungusValidato < ActiveRecord::Validator
-  def validate
-    uf = UserFungus.where(user_game: record.user_game, x: record.x, y: record.y).first(lock: true)
-    unless uf.nil?
-      record.errors[:x] << "Exists UserFungus"
-    end
-  end
-end
